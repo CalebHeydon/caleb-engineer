2d84fe2f33e2d7ada67962accf9aa58e0189cce4
Caleb Heydon <caleb.heydon@gmail.com>
2024-05-01 23:08:02 -0400

    Added Zed page

---
+++ test.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+PORT = 8080
+DIRECTORY = "html"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
